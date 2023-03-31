e30d4aafeef25ef5f748ec8a4298e1e251bc85ef
Ananya <17ananya.benjwal@gmail.com>
2023-03-31 17:31:09 +0530

    Initial commit

---
+++ Stage2.py
@@ -0,0 +1,9 @@
+print("Write how many cups of coffee you will need:")
+num_cup = int(input())
+num_water = num_cup * 200;
+num_milk = num_cup * 50
+num_beans = num_cup * 15
+print("For " + str(num_cup) + " cups of coffee you will need:")
+print(str(num_water) + " ml of water")
+print(str(num_milk) + " ml of milk")
+print(str(num_beans) + " ml of beans")
---
+++ Stage4.py
@@ -0,0 +1,118 @@
+pre_water = 400
+pre_milk = 540
+pre_beans = 120
+pre_cups = 9
+pre_money = 550
+print("The coffee machine has:")
+print(str(pre_water) + " ml of water")
+print(str(pre_milk) + " ml of milk")
+print(str(pre_beans) + " g of beans")
+print(str(pre_cups) + " disposable cups")
+print("$" + str(pre_money) + " of money")
+
+print("Write action (buy, fill, take):")
+
+action = input();
+if action == "take":
+    print("I gave you $" + str(pre_money))
+    pre_money = 0
+elif action == "buy":
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    command = int(input())
+    if command == 1:
+        esp_water = 250
+        esp_beans = 16
+        esp_money = 4
+        esp_cup = 1
+        if pre_water > esp_water and pre_beans > esp_beans and pre_cups > esp_cup:
+            pre_water = pre_water - esp_water
+            pre_beans = pre_beans - esp_beans
+            pre_cups = pre_cups - 1
+            pre_money = pre_money + esp_money
+        else:
+            if pre_water < esp_water:
+                print("Sorry, not enough water!")
+            elif esp_beans > pre_beans:
+                print("Sorry, not enough beans!")
+            elif esp_cup > pre_cups:
+                print("Sorry, not enough cups!")
+
+    elif command == 2:
+        lat_water = 350
+        lat_milk = 75
+        lat_beans = 20
+        lat_money = 7
+        if pre_water > lat_water and pre_milk > lat_milk and pre_beans > lat_beans and pre_cups > 1:
+            pre_water = pre_water - lat_water
+            pre_milk = pre_milk - lat_milk
+            pre_beans = pre_beans - lat_beans
+            pre_cups = pre_cups - 1
+            pre_money = pre_money + lat_money
+        else:
+            if pre_water < lat_water:
+                print("Sorry, not enough water!")
+                pre_water = pre_water
+            elif pre_milk < lat_milk:
+                print("Sorry, not enough milk!")
+                pre_milk = pre_milk
+            elif pre_beans < lat_beans:
+                print("Sorry, not enough beans!")
+                pre_beans = pre_beans
+            elif pre_cups < 1:
+                print("Sorry, not enough cups!")
+                pre_beans = pre_beans
+
+
+    elif command == 3:
+        cap_milk = 100
+        cap_water = 200
+        cap_beans = 12
+        cap_money = 6
+        if pre_milk > cap_milk and pre_water > cap_water and pre_beans > cap_beans and pre_cups > 1:
+            pass
+
+        else:
+            if pre_milk < cap_milk:
+                print("Sorry, not enough milk!")
+            elif pre_water < cap_water:
+                print("Sorry, not enough water!")
+            elif pre_beans < cap_beans:
+                print("Sorry, not enough beans!")
+            elif pre_cups < 1:
+                print("Sorry, not enough cups!")
+        pre_water = pre_water - cap_water
+        pre_milk = pre_milk - cap_milk
+        pre_beans = pre_beans - cap_beans
+        pre_cups = pre_cups - 1
+        pre_money = pre_money + cap_money
+
+
+elif action == "fill":
+    print("Write how many ml of water you want to add:");
+    add_water = int(input())
+    print("Write how many ml of milk you want to add:");
+    add_milk = int(input())
+    print("Write how many grams of coffee beans you want to add:");
+    add_beans = int(input())
+    print("Write how many disposable cups of coffee you want to add:");
+    add_cups = int(input())
+    pre_water = pre_water + add_water
+    pre_milk = pre_milk + add_milk
+    pre_beans = pre_beans + add_beans
+    pre_cups = pre_cups + add_cups
+    print("The coffee machine has:")
+    print(str(pre_water) + " ml of water")
+    print(str(pre_milk) + " ml of milk")
+    print(str(pre_beans) + " g of beans")
+    print(str(pre_cups) + " disposable cups")
+    print("$" + str(pre_money) + " of money")
+    exit()
+
+print(" ")
+print(" ")
+print("The coffee machine has:")
+print(str(pre_water) + " ml of water")
+print(str(pre_milk) + " ml of milk")
+print(str(pre_beans) + " g of beans")
+print(str(pre_cups) + " disposable cups")
+print("$" + str(pre_money) + " of money")
